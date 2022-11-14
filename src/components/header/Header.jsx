66c9430a22bef41c71a22e5180923a66973b4f63
anysound <anysoundspb@gmail.com>
2022-11-15 00:57:38 +0300

    fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import { Button } from '../button/Button'
+import React, { useEffect } from "react";
+import { Button } from "../button/Button";
 
 export const Header = () => {
-const tg = window.Telegram.WebApp;
- 
-    const onClose = () => {
-      tg.close();
-    };
+  const tg = window.Telegram.WebApp;
 
-    useEffect(() => {
-      tg.ready();
-    }, []);
+  const onClose = () => {
+    tg.close();
+  };
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
   return (
-    <div className='header'>
-        <Button onClick={onClose}>Закрыть</Button>
-        <span>
-          {tg.initDataUnsafe?.user?.username}
-        </span>
+    <div className="header">
+      <Button onClick={onClose}>Закрыть</Button>
+      <span>{tg.initDataUnsafe?.user?.username}</span>
     </div>
-  )
-}
+  );
+};
