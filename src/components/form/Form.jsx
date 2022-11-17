13cff5b23a1706f1d5031ef1f3b452768b6d7c6b
anysound <anysoundspb@gmail.com>
2022-11-17 23:05:27 +0300

    commit

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./Form.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
@@ -6,27 +6,61 @@ export const Form = () => {
   const [country, setCountry] = useState("");
   const [street, setStreet] = useState("");
   const [subject, setSubject] = useState("");
-  const {tg} = useTelegram()
-  
+  const { tg } = useTelegram();
+
   useEffect(() => {
     tg.MainButton.setParams({
-      text: "Отправить данные"
-    })
+      text: "Отправить данные",
+    });
   }, [tg.MainButton]);
 
-    useEffect(() => {
-      if (!street || !country) {
-        tg.MainButton.hide();
-      } else {
-        tg.MainButton.show()
-      }
-    }, [country, street, tg.MainButton]);
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street, tg.MainButton]);
+
+  const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject
+    };
+    tg.sendData(JSON.stringify(data))
+  }, [country, street, subject, tg])
+
+  useEffect(() => {
+    tg.WebApp.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.WebApp.offEvent('mainButtonClicked', onSendData);
+    }
+  }, [onSendData, tg.WebApp])
 
   return (
     <div className="form">
-      <input type="text" className="input" placeholder="страна" value={country} onChange={(e) => setCountry(e.target.value)} />
-      <input type="text" className="input" placeholder="улица" value={street} onChange={(e) => setStreet(e.target.value)} />
-      <select name="" id="" className="select" onChange={(e) => setSubject(e.target.value)} value={subject}>
+      <input
+        type="text"
+        className="input"
+        placeholder="страна"
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+      />
+      <input
+        type="text"
+        className="input"
+        placeholder="улица"
+        value={street}
+        onChange={(e) => setStreet(e.target.value)}
+      />
+      <select
+        name=""
+        id=""
+        className="select"
+        onChange={(e) => setSubject(e.target.value)}
+        value={subject}
+      >
         <option value="legal">юр. лицо</option>
         <option value="physical">физ. лицо</option>
       </select>
